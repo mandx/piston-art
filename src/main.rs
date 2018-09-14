1e815cb285e564835fa3840ede7edb382349fd08
Armando Perez <armando@roastery.com>
2018-09-14 16:59:59 -0700

    Looking more like an inverted Schotter now

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,49 @@ use rand::prelude::*;
 use sdl2_window::Sdl2Window;
 
 fn draw_rect(
-    slot_x: usize,
-    slot_y: usize,
+    slot_x: i32,
+    slot_y: i32,
     factor: f64,
     radius: f64,
     context: &piston_window::Context,
     graphics: &mut G2d,
 ) {
-    let start_x = (slot_x as f64) * radius * 2.0;
-    let start_y = (slot_y as f64) * radius * 2.0;
-    let end_x = start_x + radius * 2.0;
-    let end_y = start_y + radius * 2.0;
+    let rect_width = radius * 2.0;
+    let start_x = f64::from(slot_x) * rect_width;
+    let start_y = f64::from(slot_y) * rect_width;
+    // let color = [random(), random(), random(), 1.0];
+    let color = [0.0, 0.0, 0.0, 0.75];
+    let transform = context
+        .transform
+        .trans(start_x + radius, start_y + radius)
+        .rot_rad(factor);
 
-    rectangle(
-        [random(), random(), random(), 1.0], // red
-        [start_x, start_y, end_x, end_y],    // rectangle
-        context
-            .transform
-            // .clone()
-            // .trans(start_x + radius, start_y + radius)
-            .rot_rad(factor),
+    line(
+        color,
+        1.0,
+        [-radius, -radius, radius, -radius],
+        transform,
+        graphics,
+    );
+    line(
+        color,
+        1.0,
+        [radius, -radius, radius, radius],
+        transform,
+        graphics,
+    );
+    line(
+        color,
+        1.0,
+        [radius, radius, -radius, radius],
+        transform,
+        graphics,
+    );
+    line(
+        color,
+        1.0,
+        [-radius, radius, -radius, -radius],
+        transform,
         graphics,
     );
 }
@@ -47,16 +70,22 @@ fn main() {
                 .map(|v| (v.rect[2], v.rect[3]))
                 .unwrap_or((640, 480));
 
-            let blocks_count_x: usize = 6;
-            let blocks_count_y: usize = 6;
-            let radius: f64 = (viewport_width as f64) / ((blocks_count_x as f64) * 2.0);
+            let blocks_count_x: i32 = 6;
+            let blocks_count_y: i32 = viewport_height / blocks_count_x;
+            let radius: f64 = f64::from(viewport_width) / (f64::from(blocks_count_x) * 2.0);
 
             for slot_x in 0..blocks_count_x {
                 for slot_y in 0..blocks_count_y {
                     draw_rect(
                         slot_x,
                         slot_y,
-                        random(), // factor
+                        random::<f64>()
+                            * (
+                                1.0
+                                /
+                                (1.0 + f64::from(slot_y))
+                            )
+                            * (if random() { 1.0 } else { -1.0 }), // factor
                         radius,
                         &context,
                         graphics,
